52a64f2629888a22baa350ab0a1fc332890b6803
buddydip <buddydip@gmail.com>
2024-01-14 15:50:18 +0530

    Update GestureDetection.py

--- Code/Ch07/Python/GestureDetection.py
+++ Code/Ch07/Python/GestureDetection.py
@@ -5,18 +5,40 @@ from cvzone.HandTrackingModule import HandDetector
 from picamera2 import Picamera2
 
 
+import json
+import paho.mqtt.client as mqtt
+
+
+MQTT_ADDRESS:str = 'smarty.local'
+MQTT_TOPIC_SWITCH: str = 'smarty/switchcontrol/mainstudy'
+MQTT_CLIENT_ID:str = 'MQTTDataBridge'
+MQTT_PORT:int = 1883
+
+switchID1 = 'MAINSTUDY.Switch4'
+switchID2 = 'MAINSTUDY.Switch3'
+
+    
+def _publish_message(topic, switchID, switchState):
+    data = {}
+    data['SwitchID'] = switchID
+    data['SwitchState'] = switchState
+    payload = json.dumps(data)
+    print(payload)
+    mqtt_client.publish(MQTT_TOPIC_SWITCH, payload)
+    return
+ 
+
+
+mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
+mqtt_client.connect(MQTT_ADDRESS, MQTT_PORT)
+mqtt_client.loop_start()  # start the loop
+
 
 ptime = 0
 ctime = 0
 switch1 = False
 switch2 = False
-switchpin1 = 26
-switchpin2 = 17
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(switchpin1, GPIO.OUT)    # Switch 1
-GPIO.setup(switchpin2, GPIO.OUT)    # Switch 2
-GPIO.setwarnings(False)
 
 picam2 = Picamera2()
 picam2.preview_configuration.main.size = (980,460)
@@ -40,22 +62,14 @@ while True :
             print(fingerUp)
             if fingerUp == [1, 0, 0, 0, 0] :
                 print("Switch 1 toggled")
-                if switch1 == False:
-                    GPIO.output(switchpin1, GPIO.HIGH)
-                    switch1 = True
-                else :
-                    GPIO.output(switchpin1, GPIO.LOW)
-                    switch1 = False
+                _publish_message(MQTT_TOPIC_SWITCH, switchID1, not(switch1))
+                switch1 = not(switch1)
                 time.sleep(2)
                 
             if fingerUp == [0, 1, 0, 0, 0] :
                 print("Switch 2 toggled")
-                if switch2 == False:
-                    GPIO.output(switchpin2, GPIO.HIGH)
-                    switch2 = True
-                else :
-                    GPIO.output(switchpin2, GPIO.LOW)
-                    switch2 = False
+                _publish_message(MQTT_TOPIC_SWITCH, switchID2, not(switch2))
+                switch2 = not(switch2)
                 time.sleep(2)
         
     ctime = time.time()
@@ -70,3 +84,4 @@ while True :
 GPIO.cleanup()
     
 cv2.destroyAllWindows()
+
