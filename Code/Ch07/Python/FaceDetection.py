6847c74f94b1950b418adc68fe9ed39077b41097
buddydip <buddydip@gmail.com>
2024-01-14 13:17:00 +0530

    Update FaceDetection.py

--- Code/Ch07/Python/FaceDetection.py
+++ Code/Ch07/Python/FaceDetection.py
@@ -1,57 +1,82 @@
+import cvzone
+from cvzone.FaceDetectionModule import FaceDetector
 import cv2
-import numpy as np
-import RPi.GPIO as GPIO
-import time
 from picamera2 import Picamera2
 
-switchPin =26
+import json
+import paho.mqtt.client as mqtt
 
-# GPIO setup
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(switchPin, GPIO.OUT)
-GPIO.setwarnings(False)
 
-# Load pre-trained MobileNet SSD model for face detection
-net = cv2.dnn.readNetFromCaffe('/home/pi/RPi/Code/Python/model/deploy.prototxt', '/home/pi/RPi/Code/Python/model/mobilenet_iter_73000.caffemodel')
+MQTT_ADDRESS:str = 'smarty.local'
+MQTT_TOPIC_SWITCH: str = 'smarty/switchcontrol/mainstudy'
+MQTT_CLIENT_ID:str = 'MQTTDataBridge'
+MQTT_PORT:int = 1883
 
-# Video capture setup
+switchID = 'MAINSTUDY.Switch4'
+switchState = 1
+    
+def _publish_message(topic, switchID, switchState):
+    data = {}
+    data['SwitchID'] = switchID
+    data['SwitchState'] = switchState
+    payload = json.dumps(data)
+    print(payload)
+    mqtt_client.publish(MQTT_TOPIC_SWITCH, payload)
+    return
+ 
+
+
+mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
+mqtt_client.connect(MQTT_ADDRESS, MQTT_PORT)
+mqtt_client.loop_start()  # start the loop
+
+
+# Initialize the webcam
 picam2 = Picamera2()
 picam2.preview_configuration.main.size = (980,460)
 picam2.preview_configuration.main.format = "RGB888"
 picam2.preview_configuration.align()
 picam2.configure("preview")
 picam2.start()
+    
+# Initialize the FaceDetector object
+# minDetectionCon: Minimum detection confidence threshold
+# modelSelection: 0 for short-range detection (2 meters), 1 for long-range detection (5 meters)
+detector = FaceDetector(minDetectionCon=0.5, modelSelection=0)
 
+# Run the loop to continually get frames from the webcam
 while True:
-    # Read a frame from the camera
+    # Read the current frame from the webcam
+    # success: Boolean, whether the frame was successfully grabbed
+    # img: the captured frame
     img = picam2.capture_array()
 
-    # Prepare the frame for face detection
-    blob = cv2.dnn.blobFromImage(img, 0.007843, (300, 300), 127.5)
-    net.setInput(blob)
+    # Detect faces in the image
+    # img: Updated image
+    # bboxs: List of bounding boxes around detected faces
+    img, bboxs = detector.findFaces(img, draw=False)
 
-    # Perform face detection
-    detections = net.forward()
+    # Check if any face is detected
+    if bboxs:
+        # Loop through each bounding box
+        for bbox in bboxs:
+            # bbox contains 'id', 'bbox', 'score', 'center'
 
-    # Check for face presence in the detected regions
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-        if confidence > 0.5:  # Adjust the confidence threshold as needed
-            GPIO.output(switchPin, GPIO.HIGH)
-            print("Face detected - Relay ON")
-            time.sleep(1)
-            break
-        else:
-            GPIO.output(switchPin, GPIO.LOW)
-            print("No face detected - Relay OFF")
+            # ---- Get Data  ---- #
+            center = bbox["center"]
+            x, y, w, h = bbox['bbox']
+            score = int(bbox['score'][0] * 100)
+            
+            #publish message on face detection
+            if score > 95:
+                _publish_message(MQTT_TOPIC_SWITCH, switchID, switchState)
+            
+            # ---- Draw Data  ---- #
+            cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+            cvzone.putTextRect(img, f'{score}%', (x, y - 10))
+            cvzone.cornerRect(img, (x, y, w, h))
 
-    # Display the frame
-    cv2.imshow("Face Detection", img)
-
-    # Break the loop if 'Esc' key is pressed
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
-
-# Cleanup GPIO and release resources
-GPIO.cleanup()
-cv2.destroyAllWindows()
+    # Display the image in a window named 'Image'
+    cv2.imshow("Image", img)
+    # Wait for 1 millisecond, and keep the window open
+    cv2.waitKey(1)
